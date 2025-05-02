ba3996c0cd7e2b64b34010f0f6a7c0f5fced3077
KingCuddles314 <94393717+7700picasso@users.noreply.github.com>
2025-05-01 19:33:30 -0700

    Update main.cpp
    ...

--- Joshua/src/main.cpp
+++ Joshua/src/main.cpp
@@ -44,6 +44,7 @@ struct robot{
   float x = 0;
   float y= 0;
   float heading = 0;
+  Robot(double x_init = 0, double x)
 }myRobot;
 
 void moveForward(float targetInches) {
@@ -79,20 +80,65 @@ void moveForward(float targetInches) {
   myRobot.y += targetInches *sin (myRobot.heading * (3.14/180));
 }
 
+void turnToAngle(double targetDegrees) {
+  double kp = 0.8;
+  double tolerance = 2.0;
+
+  while (true){
+    double currentHeading = IMU.heading();
+    
+    if (currentHeading > 180) currentHeading -= 360;
+    Brain.Screen.printAt(10,115,Current: %.1f, Target: %.1f, Error: %.1f);
+    while (true){
+      double error = targetDegrees - currentheading();
+  
+      while (error > 180) error -= 360;
+      while (error <-180) error += 360;
+  
+      if (fabs(error) < tolerancee) break;
+      
+    }
+    RF.spin(forward, speed, pct);
+    LF.spin(forward, speed, pct);
+    LB.spin(forward, speed, pct);
+    RB.spin(forward, speed, pct);
+
+    
+  }
+}
+
+void goToPoint(double targetX, double tagetY){
+  double dx = targetX-myRobot.x;
+  double dy-myRobot.y;
+}
+
+doubletargetAngle = atan2(dx, dy) * (180.0/3.14);
+
+if (fabs(trgetAngle - myRobt.angle)>5.0){
+  turnToAngle(targetAngle);
+}
+
+doubledistance = sqrt(dx*dx + dy*dy);
+moveForward(disance);
+
+Controller.rumble(".-.")
+
+
 void turnToTheAngle (double targetDegrees){
   double kp = 0.8;
   double tolerate = 2.0;
 
   while (true){
-    double error = targetDegrees - IMU.heading();
+    double error = targetDegrees - currentheading();
 
     while (error > 180) error -= 360;
     while (error <-180) error += 360;
 
-    if (fabs(error) < tolerate) break;
+    if (fabs(error) < tolerancee) break;
     
   }
 }
+
 
 
 float getDistanceInches () {
@@ -170,26 +216,19 @@ int turnTask(){
     }
     return 0;
 }
-void turnToAngle(float degrees){
-  desiredHeading = degrees;
-  angleTarget = degrees;
-  turnTaskActive = true;
-  while (turnTaskActive){
-    wait(10,msec);
-  }
-}
+
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
-  resetDirve();
+  /*resetDirve();
   while(IMU.isCalibrating()){
     wait(200, msec);
-  }
+}
 
 
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
-}
+*/}
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -202,7 +241,10 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  task dc (driveTask);
+
+  goToPoint(72, 24);
+  goToPoint(120,24);
+  /*task dc (driveTask);
   task tc(turnTask);
   wait(500, msec);
 
@@ -214,6 +256,7 @@ void autonomous(void) {
 
   tc.stop();
   dc.stop();
+  */
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
